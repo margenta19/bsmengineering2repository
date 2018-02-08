2d32c4c812abf2e23d9cc2c6c7a48a1af8cabaa1
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-02-08 09:57:33 -0600

    added file for number 1

---
+++ Module12#1.py
@@ -0,0 +1,24 @@
+# if sensor reads 1- turn the motor off
+# if sensor reads 0- continue
+
+# looking in python the hard way at ex 33 to figure out
+#if there is enough information there to make a while loop
+
+#script to make i the sensor must come before
+from setup import RPL
+import post_to_web as PTW # see post_to_web.py for instructions
+
+i = sensor_pin
+sensor_pin = 16
+RPL.pinMode(sensor_pin,RPL.INPUT)
+
+while i < 0:
+    RPL.servoWrite(0, 1000)
+    print "on correct"
+
+while i > 0:
+    RPL.servoWrite(0, 0)
+    print "off correct"
+
+while True:
+  PTW.state['d1'] = RPL.digitalRead(sensor_pin)
