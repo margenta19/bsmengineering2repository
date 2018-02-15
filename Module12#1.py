948ce923cc3589f83aaf96c8b0cb8d7ce60547b0
margenta19 <33258602+margenta19@users.noreply.github.com>
2018-02-15 11:23:17 -0600

    change sensor pin

--- Module12#1.py
+++ Module12#1.py
@@ -10,7 +10,7 @@ import setup
 from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
 
-sensor_pin = 16
+sensor_pin = 8
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
 while True:
