57963d0978b526074681dd4ef9465df63640ade0
gjuckniess19 <33226559+gjuckniess19@users.noreply.github.com>
2018-02-23 11:01:39 -0600

    Update Module12#1.py

--- Module12#1.py
+++ Module12#1.py
@@ -10,7 +10,7 @@ import setup
 from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
 
-sensor_pin = 8
+sensor_pin = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
 while True:
@@ -21,9 +21,8 @@ if RPL.digitalRead(sensor_pin) == 1:
     import RoboPiLib as RPL
     import setup
     RPL.servoWrite(0, 1000)
-    print "on correct"
 if RPL.digitalRead(sensor_pin) == 0:
-    import RoboPiLib
+    import RoboPiLib as RPL
     import setup
     RPL.servoWrite(0, 0)
-    print "off correct"
+    
